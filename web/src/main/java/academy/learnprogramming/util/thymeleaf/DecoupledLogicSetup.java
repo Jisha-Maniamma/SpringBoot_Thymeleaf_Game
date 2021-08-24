b074ec33aeeb7feb4b1c487446508c93a74b9f00
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-08-25 07:26:52 +0900

    Spring Boot using Thymeleaf

--- web/src/main/java/academy/learnprogramming/util/thymeleaf/DecoupledLogicSetup.java
+++ web/src/main/java/academy/learnprogramming/util/thymeleaf/DecoupledLogicSetup.java
@@ -1,4 +1,24 @@
 package academy.learnprogramming.util.thymeleaf;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@Component
 public class DecoupledLogicSetup {
+    private final SpringResourceTemplateResolver springResourceTemplateResolver;
+
+    @Autowired
+public DecoupledLogicSetup(SpringResourceTemplateResolver springResourceTemplateResolver){
+    this.springResourceTemplateResolver=springResourceTemplateResolver;
+}
+@PostConstruct
+    public void init(){
+    springResourceTemplateResolver.setUseDecoupledLogic(true);
+    log.info("Decoupled template logic enabled");
+}
 }
