bb140bbdc48f00628e27c106ea8e35b43fe12d42
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-08-25 11:50:08 +0900

    Spring Boot using Thymeleaf

--- web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,7 +1,9 @@
 package academy.learnprogramming.config;
 
+import academy.learnprogramming.interceptor.RequestInterceptor;
 import academy.learnprogramming.util.ViewName;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
        registry.addViewController("/home").setViewName(ViewName.HOME);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RequestInterceptor());
     }
 }
