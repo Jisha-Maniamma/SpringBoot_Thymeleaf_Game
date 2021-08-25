4348efca807fdcff28126e8a76189246d0eafe3f
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-08-25 13:47:52 +0900

    Spring Boot using Thymeleaf

--- web/src/main/java/academy/learnprogramming/config/WebConfig.java
+++ web/src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -2,10 +2,14 @@ package academy.learnprogramming.config;
 
 import academy.learnprogramming.interceptor.RequestInterceptor;
 import academy.learnprogramming.util.ViewName;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -17,5 +21,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new RequestInterceptor());
+        registry.addInterceptor(new LocaleChangeInterceptor());
     }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
+
+
 }
