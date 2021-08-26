953f274abb70d04c31b41776e311cf5a8a6e837c
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-08-26 09:34:18 +0900

    Spring Boot using Thymeleaf

---
+++ web/src/main/java/academy/learnprogramming/config/MainSecurityConfig.java
@@ -0,0 +1,25 @@
+package academy.learnprogramming.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class MainSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home", "/js/**", "/css/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+}
